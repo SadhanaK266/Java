2ca363c4cf93ebafc97d2e36f4dfe9481e31890c
SadhanaK266 <sadhanakaruppusamy266@gmail.com>
2025-08-10 21:39:07 +0530

    10th August'25

---
+++ Problem Solving/GeeksForGeeks/Problem3.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Problem3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println("Before swapping : a="+a+", b="+b);
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println("After swapping : a="+a+", b="+b);
+    }
+}
