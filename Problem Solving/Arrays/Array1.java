8701c6369911e6d4dd0ecc26845e37572869945b
SadhanaK266 <sadhanakaruppusamy266@gmail.com>
2025-08-13 19:04:50 +0530

    13th August'25

---
+++ Problem Solving/Arrays/Array1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Array1{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the length of an array : ");
+        int num=sc.nextInt();
+        int arr[]=new int[num];
+        System.out.print("Enter the "+num+" array elements : ");
+        for(int i=0;i<num;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.print("Enter the k value : ");
+        int k=sc.nextInt();
+
+        for(int i=0;i<arr.length;i+=k){
+            int left=i;
+            int right=Math.min((i+k)-1 , arr.length-1);
+
+            while(left< right){
+                int temp=arr[left];
+                arr[left]=arr[right];
+                arr[right]=temp;
+
+                left++;
+                right--;
+            }
+        }
+        for(int i=0;i<num;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
