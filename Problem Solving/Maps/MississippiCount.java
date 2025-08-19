1a74a7da972c61589a9f5a1b282202473533cf2a
SadhanaK266 <sadhanakaruppusamy266@gmail.com>
2025-08-19 10:17:17 +0530

    19th August'25

---
+++ Problem Solving/Maps/MississippiCount.java
@@ -0,0 +1,27 @@
+// import java.util.*;
+
+// public class MississippiCount {
+//     public static void main(String args[]){
+//         Scanner sc=new Scanner(System.in);
+//         String s=new String();
+//         sc.nextLine();
+//         countOccurence(String s);
+//         LinkedHashMap<Character,Integer> map=new LinkedHashMap<Character,Integer>();
+
+//         for(int i=0;i<s.length();i++){
+//             if(map.containsKey(s.charAt(i))){
+//                 int c=map.get(s.charAt(i));
+//                 map.put(s.charAt(i),c+1);
+//             }
+//             else{
+//                 map.put(s.charAt(i),1);
+//             }
+//         }
+//         Set<Entry<Character,Integer>> entries=map.entrySet();
+
+//         for(Entry<Character,Integer> e: entries){
+//             System.out.print(e.getKey() +" ");//e.getValue());
+//         }
+        
+//     }
+// }
