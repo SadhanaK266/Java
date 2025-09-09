2bcc77144cb7a080eaa80fca815d84cc4bc704af
SadhanaK266 <sadhanakaruppusamy266@gmail.com>
2025-09-09 09:35:36 +0530

    9th Sept'25

---
+++ Problem Solving/GeeksForGeeks/Person.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Person{
+    private String fName;
+    private  String LName;
+
+    public Person(String fName,String LName){
+        this.fName=fName;
+        this.LName=LName;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+    }
+}
