0d491d6a7a36e4798ef1da2d99dee252ec03305b
SadhanaK266 <sadhanakaruppusamy266@gmail.com>
2025-09-09 10:11:34 +0530

    Changes Committed

--- Visteon Practice/Factorial.java
+++ Visteon Practice/Factorial.java
@@ -2,15 +2,31 @@ import java.util.*;
 public class Factorial{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
-        System.out.print("Enter a number : ");
-        int n=sc.nextInt();
-        System.out.println(factorial(n));
-
-    }
-    public static int factorial(int n){
-        if(n==0) return 0;
-        if(n==1) return 1;
+        System.out.print("Enter the string : ");
+        String str=sc.nextLine();
+        // String normalized=str.toLowerCase();
+        // or For a line
+        String normalized = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        System.out.println(normalized);
+        String sb=new StringBuilder(normalized).reverse().toString();
+        System.out.println(sb);
         
-        return n*factorial(n-1);
+        if(normalized.equals(sb)){
+            System.out.println("Palindrome");
+        }
+        else{
+            System.out.println("Not a palindrome");
+        }
     }
+    //     System.out.print("Enter a number : ");
+    //     int n=sc.nextInt();
+    //     System.out.println(factorial(n));
+
+    // }
+    // public static int factorial(int n){
+    //     if(n==0) return 0;
+    //     if(n==1) return 1;
+        
+    //     return n*factorial(n-1);
+    // }
 }
