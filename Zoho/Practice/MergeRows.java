8bf72f7b36b3aee716b3ae5f1781111e9bac46c3
SadhanaK266 <sadhanakaruppusamy266@gmail.com>
2025-10-01 20:40:26 +0530

    committed

---
+++ Zoho/Practice/MergeRows.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class MergeRows {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[][] arr=new int[n][2];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<2;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+    }
+}
