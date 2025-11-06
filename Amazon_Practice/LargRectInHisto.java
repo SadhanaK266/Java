1049062120a972ff09b925860a6a8a1a04fc041f
SadhanaK266 <sadhanakaruppusamy266@gmail.com>
2025-11-06 14:14:21 +0530

    Largest Rectangle In the Histogram

--- Amazon_Practice/LargRectInHisto.java
+++ Amazon_Practice/LargRectInHisto.java
@@ -5,11 +5,10 @@ public class LargRectInHisto {
         System.out.print("Enter the value for n : ");
         int n=sc.nextInt();
         int[] arr=new int[n];
-        System.out.print("Enter the "+n+" Elements");
+        System.out.print("Enter the "+n+" Elements :");
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
-        int max=Integer.MIN_VALUE;
         // for(int i=0;i<n;i++){
         //     for(int j=i;j<n;j++){
         //         int min=Integer.MAX_VALUE;
@@ -24,11 +23,20 @@ public class LargRectInHisto {
         //     }
         // }
         // System.out.println("Area:"+max);
+
+        int max=Integer.MIN_VALUE;
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
-                
+                    int min=Integer.MAX_VALUE;
+                    for(int k=i;k<=j;k++){
+                        if(arr[k]<min){
+                            min=arr[k];
+                        }
+                    }
+                    max=Math.max(max,(min*((j-i)+1)));
             }
         }
+        System.out.print(max);
     }
 
 }
