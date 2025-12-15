3682935f703ce891b83da1d1ee6187d096c7b1da
SadhanaK266 <sadhanakaruppusamy266@gmail.com>
2025-12-15 16:57:55 +0530

    Program to find the difference btw elements at odd index and even index.

---
+++ CTS(Cognizant)/Prg1.java
@@ -0,0 +1,24 @@
+// Prg to find the difference btw elements at odd index and even index.
+import java.util.*;
+public class Prg1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the value for n : ");
+        int n=sc.nextInt();
+        System.out.print("Enter the "+n+" array elements : ");
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int oddSum=0, evenSum=0;
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                evenSum+=arr[i];
+            }
+            else{
+                oddSum+=arr[i];
+            }
+        }
+        System.out.print(Math.abs(oddSum-evenSum));
+    }
+}
