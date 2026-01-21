445b6600f47bf140996df6e5a7b55102dca32a27
SadhanaK266 <sadhanakaruppusamy266@gmail.com>
2026-01-21 20:00:58 +0530

    Palindrome String

---
+++ CTS(Cognizant)/Palindrome.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the String : ");
+        String str=sc.nextLine();
+        str=str.toLowerCase();
+        String rev="";
+
+        for(int i=str.length()-1;i>=0;i--){
+            rev=rev+str.charAt(i);
+        }
+        if(str.equals(rev)){
+            System.out.println("Palindrome");
+        }
+        else{
+            System.out.println("Not a Palindrome");
+        }
+    }
+}
