dd79d550d5333f28084372a3cf52671f3a0e490a
SadhanaK266 <sadhanakaruppusamy266@gmail.com>
2026-05-04 16:04:57 +0530

    updated

--- JAVA B.-A/FindMissingNumber1ton.java
+++ JAVA B.-A/FindMissingNumber1ton.java
@@ -5,7 +5,6 @@
 //int actualSum = 0;
 
 import java.util.Scanner;
-
 public class FindMissingNumber1ton {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
@@ -22,3 +21,28 @@ public class FindMissingNumber1ton {
         System.out.println("The missing number is "+Missing);
     }
 }
+ // ---------- OR ---------------------------------------------------------
+
+// public class MissingNumberXOR {
+
+//     public static void main(String[] args) {
+//         int[] arr = {1, 2, 4, 5};
+//         int n = 5;
+
+//         int xor1 = 0, xor2 = 0;
+
+//         // XOR from 1 to n
+//         for (int i = 1; i <= n; i++) {
+//             xor1 ^= i;
+//         }
+
+//         // XOR of array
+//         for (int num : arr) {
+//             xor2 ^= num;
+//         }
+
+//         int missing = xor1 ^ xor2;
+
+//         System.out.println("Missing number: " + missing);
+//     }
+// }
